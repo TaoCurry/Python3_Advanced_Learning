86241f96a8389becb5bd3789a9efd39597a479dd
e <493921441@qq.com>
2017-10-19 21:26:21 +0800

    EasyGUI的用法

--- Use_GUI/use_EasyGUI.py
+++ Use_GUI/use_EasyGUI.py
@@ -4,13 +4,13 @@
 Use EasyGUI module
 """
 __author__ = "Curry"
-# model
-# import easygui
-# user_response = easygui.msgbox('Hello World!')
+# model 1
+import easygui
+user_response = easygui.msgbox('Hello World!')
 # easygui.msgbox()创建一个消息框
-# print(user_response)
+print(user_response)
 # OK
-
+# model 2
 import easygui
 flavor = easygui.buttonbox('What\'s your favorte ice cream flavor?', choices= ['Vanilla', 'Chocolate', 'Strawberry'])
 easygui.msgbox('You picked ' + flavor)
