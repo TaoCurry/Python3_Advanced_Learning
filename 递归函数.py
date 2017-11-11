eb122f5749749115a6a65ad3262181994d02e498
XiaTao <32433843+TaoCurry@users.noreply.github.com>
2017-11-11 21:21:04 +0800

    Update 递归函数.py

--- 递归函数.py
+++ 递归函数.py
@@ -6,14 +6,8 @@
 #每当函数返回，栈就会减一层栈帧。由于栈的大小不是无限的，所以，递归调用的次数过多，会导致栈溢出。
 #使用递归函数的优点是逻辑简单清晰，缺点是过深的调用会导致栈溢出。
 
-#求n! 1*2
-#1、使用迭代
-def Fact(n):
-	result = n
-	for i in range(1,n):
-		result * = i
-	return result
-	
+#  求n! 1*2
+#  1、使用迭代
 def fact(n):
        result = n
        for i in range(1,n):
@@ -24,7 +18,8 @@ def fact(n):
 number = int(input('请输入一个正整数：'))
 result = fact(number)
 print('%d的阶乘是:%d'%(number,result))
-#2、使用递归 
+
+#	2、使用递归 
 def Fact(n):
 	if n == 1:
 		return 1
@@ -35,7 +30,7 @@ result = Fact(number)
 print('%d的阶乘是:%d'%(number,result))
 
 #3、经典的汉诺塔游戏
-def Hanoi(n,x,y,z):#n表示金块的数量，x、y、z表示三个柱子。
+def Hanoi(n,x,y,z):		# n表示金块的数量，x、y、z表示三个柱子。
 	if n == 1:
 		print('move',X,'-->' ,Z)
 	else:
