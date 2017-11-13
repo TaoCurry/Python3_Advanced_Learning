00bb793bb0a388d0c904527b01f8ccd76dd284bc
e <493921441@qq.com>
2017-11-13 19:39:46 +0800

    Python 错误处理

---
+++ 错误处理.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+
+"""
+__author__ = "Curry"
+
+
+def foo(s):
+    return 10 / int(s)
+
+
+def bar(s):
+    return foo(s) * 2
+
+
+def main():
+    bar('0')
+
+
+main()
